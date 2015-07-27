900a7f239f0f6cae15d5382db77869decf75148c
marccolm <marcolm485@gmail.com>
2015-07-27 14:51:39 -0500

    Creé modelo de concepto para servicios y materiales comienzo a recordar como funciona El Django

--- claand/cotizaciones/models.py
+++ claand/cotizaciones/models.py
@@ -75,3 +75,24 @@ class Pago(models.Model):
     def __str__(self):
         return str(self.id) + ": " + self.venta.cotizacion.descripcion + ": $" + \
         str(self.monto)
+
+class Concepto(models.Model):
+    is_active = models.BooleanField(default=True)
+    nombre = models.CharField(max_length=30)
+    costo = models.FloatField(default=0, validators=[MinValueValidator(0)])
+    tipo = models.CharField(max_length=10)
+    fecha_creacion = models.DateField(editable=False)
+    fecha_modificacion = models.DateField()
+
+    def save(self, *args, **kwargs):
+        """ Override de save para que sólo haya una fecha de creación,
+        y si dicha tupla se modifica, se guarde la fecha de modificación.
+        """
+        if not self.id:
+            self.fecha_creacion = datetime.today()
+        self.fecha_modificacion = datetime.today()
+        return super(Venta, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.tipo) + ": " + self.nombre + ": $" + \
+        str(self.costo)
